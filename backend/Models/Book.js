4f2956b7c76f56a68841f6b2aba30f23850cd67e
Rudra-Maity <149133824+Rudra-Maity@users.noreply.github.com>
2024-09-30 11:25:05 +0530

    Delete backend/Models directory

--- backend/Models/Book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-  name: String,
-  category: String,
-  rentPerDay: Number
-});
-
-module.exports = mongoose.model('Book', bookSchema);
